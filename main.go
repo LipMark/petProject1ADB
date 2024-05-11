df74524ac2e726857df37f7b811953a19f86ad6d
LipMark <144370265+LipMark@users.noreply.github.com>
2024-05-11 10:01:13 +0300

    templ

--- main.go
+++ main.go
@@ -5,7 +5,10 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
+	"strconv"
+	"strings"
 
 	"github.com/go-chi/chi/v5"
 )
@@ -14,7 +17,6 @@ import (
 //	"go-authentication-boilerplate/router"
 // doesn't work too
 //db
-db := auth.REQ
 // postgre don't work?
 
 // auth
@@ -33,4 +35,35 @@ func main() {
 
 	// mock
 	fmt.Println("test project")
+
+}
+
+// templ from git
+func home(rw http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		toDos := todolist.LoadAllToDo()
+
+		err := templates.ExecuteTemplate(rw, "home", types.LoadAllToDoData{ToDos: toDos})
+		if err != nil {
+			http.Error(rw, err.Error(), http.StatusBadRequest)
+		}
+		break
+	case "POST":
+		res, err := io.ReadAll(r.Body)
+		if err != nil {
+			panic(err.Error())
+		}
+		id := fmt.Sprintf("%s", res)
+		getID := strings.Split(id, "=")
+		toDoID, err := strconv.Atoi(getID[1])
+		if err != nil {
+			panic(err.Error())
+		}
+		todolist.DoneToDo(toDoID)
+		http.Redirect(rw, r, "/", http.StatusFound)
+		break
+	default:
+		break
+	}
 }

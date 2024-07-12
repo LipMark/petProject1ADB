8f05d721a4672c6da953d364178d0ab9fb4d5d0e
LipMark <144370265+LipMark@users.noreply.github.com>
2024-07-12 16:46:29 +0300

    handlers

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -1,1 +1,18 @@
 package handlers
+
+e.GET("/users", func(c echo.Context) error {
+	users := userController.GetUser() 
+	c.Bind(&users) 
+	return c.JSON(http.StatusOK, users)
+})
+
+e.POST("/users", func(c echo.Context) error {
+	userController.Create(c)
+	return c.String(http.StatusOK, "created")
+})
+
+e.DELETE("/users/:id", func(c echo.Context) error {
+	id := c.Param("id")
+	userController.Delete(id)
+	return c.String(http.StatusOK, "deleted")
+})

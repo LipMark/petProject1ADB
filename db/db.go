7c423a0e6573c2da094a4d1ae1cb3482050b7d04
LipMark <144370265+LipMark@users.noreply.github.com>
2024-07-14 23:36:15 +0300

    db bone

---
+++ db/db.go
@@ -0,0 +1,52 @@
+package db
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/jinzhu/gorm"
+)
+
+var db *gorm.DB
+var err error
+
+func Init() {
+	user := getEnv("PG_USER", "hugo")
+	password := getEnv("PG_PASSWORD", "")
+	host := getEnv("PG_HOST", "localhost")
+	port := getEnv("PG_PORT", "8080")
+	database := getEnv("PG_DB", "tasks")
+
+	dbinfo := fmt.Sprintf("user=%s password=%s host=%s port=%s dbname=%s sslmode=disable",
+		user,
+		password,
+		host,
+		port,
+		database,
+	)
+
+	db, err = gorm.Open("postgres", dbinfo)
+	if err != nil {
+		log.Println("Failed to connect to database")
+		panic(err)
+	}
+	log.Println("Database connected")
+
+	if !db.HasTable(&models.Task{}) {
+		err := db.CreateTable(&models.Task{})
+		if err != nil {
+			log.Println("Table already exists")
+		}
+	}
+
+	db.AutoMigrate(&models.Task{})
+}
+
+// GetDB ...
+func GetDB() *gorm.DB {
+	return db
+}
+
+func CloseDB() {
+	db.Close()
+}
--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module github.com/LipMark/petProject1ADB
 
 go 1.21.3
 
-require github.com/go-chi/chi/v5 v5.1.0
+require (
+	github.com/go-chi/chi/v5 v5.1.0
+	github.com/jinzhu/gorm v1.9.16
+)
+
+require github.com/jinzhu/inflection v1.0.0 // indirect
